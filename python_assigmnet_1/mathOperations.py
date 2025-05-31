82a7e18802a8ea6753fcbca318f77d999600dc41
Tushri3501 <amit.cit.cse@gmail.com>
2025-05-31 19:12:49 +0530

    Add files via upload

---
+++ python_assigmnet_1/mathOperations.py
@@ -0,0 +1,17 @@
+a=input('Enter the first number: ')
+b=input('Enter the second number: ')
+
+a=int(a)
+b=int(b)
+
+addition= a+b
+print('Addition: ',addition)
+
+Subtraction= a-b
+print('Subtraction: ',Subtraction)
+
+Multiplication= a*b
+print('Multiplication: ',Multiplication);
+
+Division = a/b
+print('Division: ',Division)
---
+++ python_assigmnet_1/personalizedGreeting.py
@@ -0,0 +1,5 @@
+n1=input('Enter your first name: ')
+n2=input('Enter your second name: ')
+
+n3 = n1+n2
+print('Hello,' ,n3, '!' 'Welcome to python program' )
