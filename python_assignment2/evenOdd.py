10d4275b1859506bedb5d1f40beab9d9a89f4db4
Tushri3501 <amit.cit.cse@gmail.com>
2025-06-07 21:44:51 +0530

    Add files via upload

---
+++ python_assignment2/evenOdd.py
@@ -0,0 +1,10 @@
+number=int(input('Enter the Number: '))
+
+if number==0:
+    print('Please enter the grater value')
+
+if number%2==0:
+    print('number is even')
+
+else:
+    print('Number is odd')
